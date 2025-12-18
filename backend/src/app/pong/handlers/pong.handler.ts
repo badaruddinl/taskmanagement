3358a69a1e545aaf950f9d223a5f0a9782d200af
Badaruddin Luthfi <badarlut@gmail.com>
2025-12-18 17:10:25 +0700

    feat: remove domain init

--- backend/src/app/pong/handlers/pong.handler.ts
+++
@@ -1,7 +0,0 @@
-import Interceptor from "@/utils/responseInterceptor.util";
-import { FastifyReply, FastifyRequest } from "fastify/fastify";
-import { StatusCodes } from "http-status-codes";
-
-export default async function pongHandler(request:FastifyRequest, reply:FastifyReply){
-    return Interceptor(reply, StatusCodes.OK,true,  request.id,"pong",)
-}
