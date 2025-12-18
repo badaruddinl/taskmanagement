4d2f245878d489b57e4c12b43cea807cc5bd522c
Badaruddin Luthfi <badarlut@gmail.com>
2025-12-18 16:59:35 +0700

    feat: add hash function

---
+++ backend/src/utils/hashData.util.ts
@@ -0,0 +1,11 @@
+import argon2 from 'argon2'
+
+export async function hashData(data: string) {
+  return argon2.hash(data, {
+    type: argon2.argon2id,
+  })
+}
+
+export async function verifyData(hash: string, plain: string) {
+  return argon2.verify(hash, plain)
+}
