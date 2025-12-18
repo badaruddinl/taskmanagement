1192bd65a37d3dc2a2a0ca7f183c38628d5efcce
Badaruddin Luthfi <badarlut@gmail.com>
2025-12-18 17:06:06 +0700

    feat: add role schema in connection

--- backend/src/connections/database.ts
+++ backend/src/connections/database.ts
@@ -1,35 +1,38 @@
-import { DataSource } from "typeorm";
-import { config } from "dotenv";
-import { User } from "@/schemas";
+import 'reflect-metadata'
 
-config();
+import { DataSource } from 'typeorm'
+import { config } from 'dotenv'
+import { User } from '@/schemas'
+import { Role } from '@/schemas/role.schema'
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+config()
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
 
 if (!DB_HOST || !DB_USER || !DB_PASSWORD || !DB_NAME) {
-  throw new Error("Missing Database Configuration in .env file");
+  throw new Error('Missing Database Configuration in .env file')
 }
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
+  type: 'mysql',
   host: DB_HOST,
   port: Number(DB_PORT) || 3306,
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-  
-  entities: [User], 
-  migrations: ["src/migrations/**/*.ts"],
+
+  entities: [User, Role],
+  migrations: ['src/migrations/**/*.ts'],
   synchronize: false,
   logging: true,
-});
+})
 
 export async function connectDatabase() {
   try {
-    await AppDataSource.initialize();
-    console.log("Data Source has been initialized!");
+    await AppDataSource.initialize()
+    console.log('Data Source has been initialized!')
   } catch (err) {
-    console.error("Error during Data Source initialization:", err);
-    throw err;
+    console.error('Error during Data Source initialization:', err)
+    throw err
   }
 }
