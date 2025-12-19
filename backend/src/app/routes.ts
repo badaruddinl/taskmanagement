518686a0f71a81b9de7474785e48e41fc0be6be9
Badaruddin Luthfi <badarlut@gmail.com>
2025-12-19 10:32:42 +0700

    feat: add route users

--- backend/src/app/routes.ts
+++ backend/src/app/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import authRoutes from './auth/routes'
 import roleRoutes from './role/routes'
+import usersRoutes from './users/routes'
 
 export default async (fastify: FastifyInstance) => {
   fastify.register(authRoutes, { prefix: '/auth' })
   fastify.register(roleRoutes, { prefix: '/roles' })
+  fastify.register(usersRoutes, { prefix: '/users' })
 }
